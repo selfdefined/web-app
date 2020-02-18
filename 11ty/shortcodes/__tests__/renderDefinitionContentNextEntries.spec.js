6120e5542d945ea0dba60c2b1969e6bc7f297af4
Oscar <o@ovl.design>
2020-02-18 17:30:33 +0100

    feat(detail pages): shortcode for browse nav markup

--- 11ty/shortcodes/__tests__/renderDefinitionContentNextEntries.spec.js
+++ 11ty/shortcodes/__tests__/renderDefinitionContentNextEntries.spec.js
@@ -5,46 +5,62 @@ import testCollection from '../../../_util/_mocks/testCollection.json';
 import renderDefinitionContentNextEntries from '../renderDefinitionContentNextEntries';
 
 test('first item', (t) => {
+  const { title, slug } = testCollection[0].data;
+
   t.is(
-    renderDefinitionContentNextEntries(testCollection[0], testCollection),
-    `<section class="definition-navigation definition-content__nav" aria-label="Browse definitions">
-    <nav class="definition-navigation" aria-label="Previous words"></nav>
-    <nav class="definition-navigation" aria-label="Next words"><ul class="definition-navigation__list"><li><a href=/definitions/ableism/>Ableism</a></li><li><a href=/definitions/barbaric/>Barbaric</a></li><li><a href=/definitions/biromantic/>Biromantic</a></li></ul></nav>
+    renderDefinitionContentNextEntries(title, slug, testCollection),
+    `<section class="definition-navigation definition__further-definitions_nav" aria-label="Browse definitions">
+    <h2 class="definition-navigation__headline">Browse</h2>
+    <div><h3 class="definition-navigation__sub-headline" id="context-nav-previous">Previous words</h3>
+      <nav class="definition-navigation__nav" aria-labelledby="context-nav-previous">
+        </nav>
+    </div>
+    <div><h3 class="definition-navigation__sub-headline" id="context-nav-next">Next words</h3>
+      <nav class="definition-navigation__nav" aria-labelledby="context-nav-next"><ul class="definition-navigation__list"><li><a href=/definitions/ableism/>Ableism</a></li><li><a href=/definitions/barbaric/>Barbaric</a></li><li><a href=/definitions/biromantic/>Biromantic</a></li></ul></nav>
+    </div>
     </section>`
   );
 });
 
 test('last item', (t) => {
-  console.log(
-    renderDefinitionContentNextEntries(
-      testCollection[testCollection.length - 1],
-      testCollection
-    )
-  );
-
+  const { title, slug } = testCollection[testCollection.length - 1].data;
   t.is(
-    renderDefinitionContentNextEntries(
-      testCollection[testCollection.length - 1],
-      testCollection
-    ),
-    `<section class="definition-navigation definition-content__nav" aria-label="Browse definitions">
-    <nav class="definition-navigation" aria-label="Previous words"><ul class="definition-navigation__list"><li><a href=/definitions/transgender/>Transgender</a></li><li><a href=/definitions/unreal/>unreal</a></li><li><a href=/definitions/white-fragility/>White Fragility</a></li></ul></nav>
-    <nav class="definition-navigation" aria-label="Next words"></nav>
+    renderDefinitionContentNextEntries(title, slug, testCollection),
+    `<section class="definition-navigation definition__further-definitions_nav" aria-label="Browse definitions">
+    <h2 class="definition-navigation__headline">Browse</h2>
+    <div><h3 class="definition-navigation__sub-headline" id="context-nav-previous">Previous words</h3>
+      <nav class="definition-navigation__nav" aria-labelledby="context-nav-previous">
+        <ul class="definition-navigation__list"><li><a href=/definitions/transgender/>Transgender</a></li><li><a href=/definitions/unreal/>unreal</a></li><li><a href=/definitions/white-fragility/>White Fragility</a></li></ul></nav>
+    </div>
+    <div><h3 class="definition-navigation__sub-headline" id="context-nav-next">Next words</h3>
+      <nav class="definition-navigation__nav" aria-labelledby="context-nav-next"></nav>
+    </div>
     </section>`
   );
 });
 
-test('throws if no item has been passed', (t) => {
+test('throws if no title has been passed', (t) => {
   const error = t.throws(() => {
     renderDefinitionContentNextEntries();
   });
 
-  t.is(error.message, 'E_NO_ENTRY');
+  t.is(error.message, 'E_NO_TITLE');
+});
+
+test('throws if no slug has been passed', (t) => {
+  const error = t.throws(() => {
+    renderDefinitionContentNextEntries('title');
+  });
+
+  t.is(error.message, 'E_NO_SLUG');
 });
 
 test('throws if no collection has been passed', (t) => {
   const error = t.throws(() => {
-    renderDefinitionContentNextEntries(testCollection[0]);
+    renderDefinitionContentNextEntries(
+      testCollection[0].data.title,
+      testCollection[0].data.slug
+    );
   });
 
   t.is(error.message, 'E_NO_COLLECTION');
--- 11ty/shortcodes/renderDefinitionContentNextEntries.js
+++ 11ty/shortcodes/renderDefinitionContentNextEntries.js
@@ -4,26 +4,39 @@ const findDefinitionContentNextItems = require('../helpers/findDefinitionContent
 const makeListItem = (item) =>
   `<li><a href=${definitionPermalink(item.slug)}>${item.title}</a></li>`;
 
-module.exports = function definitionContentNextEntries(entry, collection) {
-  if (!entry) throw new Error('E_NO_ENTRY');
+module.exports = function definitionContentNextEntries(
+  title,
+  slug,
+  collection
+) {
+  if (!title) throw new Error('E_NO_TITLE');
+  if (!slug) throw new Error('E_NO_SLUG');
   if (!collection) throw new Error('E_NO_COLLECTION');
+
+  const entry = { title, slug };
 
   const { next, previous } = findDefinitionContentNextItems(entry, collection);
 
-  return `<section class="definition-navigation definition-content__nav" aria-label="Browse definitions">
-    <nav class="definition-navigation" aria-label="Previous words">${
-      previous.length
-        ? `<ul class="definition-navigation__list">${previous
-            .map((item) => makeListItem(item))
-            .join('')}</ul>`
-        : ''
-    }</nav>
-    <nav class="definition-navigation" aria-label="Next words">${
-      next.length
-        ? `<ul class="definition-navigation__list">${next
-            .map((item) => makeListItem(item))
-            .join('')}</ul>`
-        : ''
-    }</nav>
+  return `<section class="definition-navigation definition__further-definitions_nav" aria-label="Browse definitions">
+    <h2 class="definition-navigation__headline">Browse</h2>
+    <div><h3 class="definition-navigation__sub-headline" id="context-nav-previous">Previous words</h3>
+      <nav class="definition-navigation__nav" aria-labelledby="context-nav-previous">
+        ${
+          previous.length
+            ? `<ul class="definition-navigation__list">${previous
+                .map((item) => makeListItem(item))
+                .join('')}</ul>`
+            : ''
+        }</nav>
+    </div>
+    <div><h3 class="definition-navigation__sub-headline" id="context-nav-next">Next words</h3>
+      <nav class="definition-navigation__nav" aria-labelledby="context-nav-next">${
+        next.length
+          ? `<ul class="definition-navigation__list">${next
+              .map((item) => makeListItem(item))
+              .join('')}</ul>`
+          : ''
+      }</nav>
+    </div>
     </section>`;
 };
