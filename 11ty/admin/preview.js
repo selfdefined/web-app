7e82467af750a4c3700e5987b009291c6ae846fd
Ned Zimmerman <ned@bight.dev>
2021-02-27 14:46:31 -0400

    Complete dynamic preview capability (exclude preview.js from AVA testing)

--- 11ty/admin/preview.js
+++ 11ty/admin/preview.js
@@ -1,65 +1,173 @@
 /* global CMS, createClass, h */
+import htm from 'https://unpkg.com/htm?module';
+
+const html = htm.bind(h);
+const md = window.markdownit({
+  html: true,
+  linkify: true,
+  typographer: true
+});
+
+const generateFlag = function(flag) {
+  const cleanText = new Map([
+    [
+      'avoid',
+      {
+        class: 'avoid',
+        text: 'Avoid'
+      }
+    ],
+    [
+      'better-alternative',
+      {
+        class: 'better',
+        text: 'Better alternate'
+      }
+    ],
+    [
+      'tool',
+      {
+        class: 'tool',
+        text: ''
+      }
+    ],
+    [
+      'warning',
+      {
+        class: 'warning',
+        text: ''
+      }
+    ]
+  ]);
+
+  if (flag) {
+    const info = cleanText.get(flag.level.toLowerCase());
+
+    const sep = flag.text && info.text ? '—' : '';
+    const text = flag.text ? [info.text, flag.text].join(sep) : info.text;
+
+    return html`
+      <p
+        class="definition-content__signal definition-content__signal--${info.class}"
+      >
+        ${text}
+      </p>
+    `;
+  }
+
+  return html`
+    <p class="definition-content__signal"></p>
+  `;
+};
+
+const decodeHtml = function(html) {
+  const txt = document.createElement('textarea');
+  txt.innerHTML = html;
+  return txt.value;
+};
 
 const DefinitionPreview = createClass({
   render: function() {
-    const entry = this.props.entry;
-    return h(
-      'main',
-      {},
-      h(
-        'article',
-        { className: 'definition' },
-        h(
-          'header',
-          { className: 'definition__header' },
-          h(
-            'p',
-            {
-              className: entry.getIn(['data', 'flag'])
-                ? `definition-content__signal definition-content__signal--${entry
-                    .getIn(['data', 'flag', 'level'])
-                    .toLowerCase()
-                    .replace('better-alternative', 'better')}`
-                : 'definition-content__signal'
-            },
-            entry.getIn(['data', 'flag', 'text'])
-              ? [
-                  entry
-                    .getIn(['data', 'flag', 'level'])
-                    .replace('better-alternative', 'Better alternate'),
-                  entry.getIn(['data', 'flag', 'text'])
-                ].join('—')
-              : entry
-                  .getIn(['data', 'flag', 'level'])
-                  .replace('better-alternative', 'Better alternate')
-          ),
-          h(
-            'h1',
-            { className: 'main-headline' },
-            entry.getIn(['data', 'title']) || 'Untitled'
-          ),
-          h(
-            'section',
-            { className: 'u-margin-bottom-double' },
-            h(
-              'p',
-              { className: 'definition-content__speech' },
-              entry.getIn(['data', 'speech']) || 'Unknown part of speech'
-            )
-          )
-        ),
-        // TODO: Add content warning flag if applicable.
-        h(
-          'section',
-          { className: 'definition-content' },
-          h(
-            'section',
-            { className: 'definition-content__content' },
-            this.props.widgetFor('body') // TODO: display as direct descendant of parent element
-          )
-        )
-      )
+    let definitionFlag,
+      contentWarning,
+      altWords,
+      furtherReading = '';
+
+    const flag = {
+      level: this.props.entry.getIn(['data', 'flag', 'level']),
+      text: this.props.entry.getIn(['data', 'flag', 'text'])
+    };
+
+    definitionFlag = generateFlag(flag);
+
+    switch (flag.level) {
+      case 'avoid':
+        contentWarning = html`
+          <div class="u-margin-bottom-double content-flag content-flag--avoid">
+            <p class="small">
+              We would recommend adding a content warning when speaking about
+              this term. Please
+              <a href="/content-warning-guidelines/">read the guidance</a> on
+              how and when to warn people before using this term in any context.
+            </p>
+          </div>
+        `;
+        break;
+      case 'warning':
+        contentWarning = html`
+          <div class="u-margin-bottom-double content-flag">
+            <p class="small">
+              We would recommend adding a content warning when speaking about
+              this term. Please
+              <a href="/content-warning-guidelines/">read the guidance</a> on
+              how and when to warn people before using this term in any context.
+            </p>
+          </div>
+        `;
+        break;
+    }
+
+    const body = decodeHtml(
+      md.render(this.props.entry.getIn(['data', 'body']))
     );
+
+    altWords =
+      this.props.entry.getIn(['data', 'alt_words'], false) &&
+      html`
+        <section className="definition-content__content">
+          <h2 id="alt-words">Alt Words</h2>
+          <ul class="list-semicolon" aria-labelledby="alt-words">
+            ${this.props.widgetsFor('alt_words').map(
+              (word, index) => html`
+                <li key="${index}">${word.getIn(['data'])}</li>
+              `
+            )}
+          </ul>
+        </section>
+      `;
+
+    furtherReading =
+      this.props.entry.getIn(['data', 'reading'], false) &&
+      html`
+      <section className="definition-content__content"> <h2 id="alt-words">Alt Words</h2>
+        <ul class="list-semicolon" aria-labelledby="alt-words">
+          ${this.props.widgetsFor('reading').map(
+            (link, index) => html`
+              <li key="${index}">
+                <a href="${link.getIn(['data', 'link'])}"
+                  >${link.getIn(['data', 'text'])}</a
+                >
+              </li>
+            `
+          )}
+        </ul>
+      </section
+      `;
+
+    return html`
+      <main>
+        <article className="definition">
+          <header className="definition__header">
+            ${definitionFlag}
+            <h1 className="main-headline">
+              ${this.props.entry.getIn(['data', 'title'])}
+            </h1>
+            <section className="u-margin-bottom-double">
+              <p className="definition-content__speech">
+                ${this.props.entry.getIn(['data', 'speech'])}
+              </p>
+            </section>
+          </header>
+          ${contentWarning}
+          <section className="definition-content">
+            <section className="definition-content__content">
+              ${html([body])}
+            </section>
+            ${altWords} ${furtherReading}
+          </section>
+        </article>
+      </main>
+    `;
   }
 });
 
